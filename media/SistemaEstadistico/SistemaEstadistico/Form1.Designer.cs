fdac14f135f308feb1432df9b2824ee147de759c
Jorge Solano <jorgesolanomelendez@gmail.com>
2026-09-01 19:28:17 -0600

    sistema estadistico

--- media/SistemaEstadistico/SistemaEstadistico/Form1.Designer.cs
+++ media/SistemaEstadistico/SistemaEstadistico/Form1.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class Form1
     {
-        /// <summary>
-        /// Variable del diseñador necesaria.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Limpiar los recursos que se estén usando.
-        /// </summary>
-        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,84 +15,266 @@
 
         #region Código generado por el Diseñador de Windows Forms
 
-        /// <summary>
-        /// Método necesario para admitir el Diseñador. No se puede modificar
-        /// el contenido de este método con el editor de código.
-        /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.listBox1 = new System.Windows.Forms.ListBox();
             this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Seleccionar Dataset";
             // 
             // comboBox1
             // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(113, 34);
+            this.comboBox1.Location = new System.Drawing.Point(30, 40);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(262, 24);
-            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Size = new System.Drawing.Size(200, 23);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ingresar Datos";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(113, 97);
+            this.textBox1.Location = new System.Drawing.Point(30, 100);
+            this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 1;
+            this.textBox1.Size = new System.Drawing.Size(200, 60);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(113, 296);
+            this.button1.Location = new System.Drawing.Point(30, 175);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 30);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Calcular";
             this.button1.UseVisualStyleBackColor = true;
-            // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(113, 169);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(120, 84);
-            this.listBox1.TabIndex = 3;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(422, 313);
+            this.button2.Location = new System.Drawing.Point(140, 175);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 30);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Limpiar";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // button3
+            // label3 (Título Media)
             // 
-            this.button3.Location = new System.Drawing.Point(559, 325);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 5;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(260, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Media Aritmética";
+            // 
+            // textBox2 (Resultado Media)
+            // 
+            this.textBox2.Location = new System.Drawing.Point(260, 40);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(110, 23);
+            this.textBox2.TabIndex = 15;
+            // 
+            // label4 (Título Mediana)
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(390, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Mediana";
+            // 
+            // textBox3 (Resultado Mediana)
+            // 
+            this.textBox3.Location = new System.Drawing.Point(390, 40);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(110, 23);
+            this.textBox3.TabIndex = 16;
+            // 
+            // label5 (Título Moda)
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(520, 20);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 15);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Moda";
+            // 
+            // textBox4 (Resultado Moda)
+            // 
+            this.textBox4.Location = new System.Drawing.Point(520, 40);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(110, 23);
+            this.textBox4.TabIndex = 17;
+            // 
+            // label6 (Título Varianza)
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(260, 75);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 15);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Varianza";
+            // 
+            // textBox5 (Resultado Varianza)
+            // 
+            this.textBox5.Location = new System.Drawing.Point(260, 95);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(110, 23);
+            this.textBox5.TabIndex = 18;
+            // 
+            // label7 (Título Desviación Estándar)
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(390, 75);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(111, 15);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Desviación Estándar";
+            // 
+            // textBox6 (Resultado Desviación Estándar)
+            // 
+            this.textBox6.Location = new System.Drawing.Point(390, 95);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(110, 23);
+            this.textBox6.TabIndex = 19;
+            // 
+            // label8 (Título Desviación Típica)
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(520, 75);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(95, 15);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Desviación Típica";
+            // 
+            // textBox7 (Resultado Desviación Típica)
+            // 
+            this.textBox7.Location = new System.Drawing.Point(520, 95);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.Size = new System.Drawing.Size(110, 23);
+            this.textBox7.TabIndex = 20;
+            // 
+            // label9 (Título Rango)
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(390, 130);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(41, 15);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "Rango";
+            // 
+            // textBox8 (Resultado Rango)
+            // 
+            this.textBox8.Location = new System.Drawing.Point(390, 150);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.ReadOnly = true;
+            this.textBox8.Size = new System.Drawing.Size(110, 23);
+            this.textBox8.TabIndex = 21;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(30, 220);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(200, 160);
+            this.dataGridView1.TabIndex = 13;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(250, 190);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(380, 190);
+            this.chart1.TabIndex = 14;
+            this.chart1.Text = "chart1";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button3);
+            this.ClientSize = new System.Drawing.Size(660, 400);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "SistemaEstadisticoUGB";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -107,12 +282,27 @@
 
         #endregion
 
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
-
--- media/SistemaEstadistico/SistemaEstadistico/Form1.cs
+++ media/SistemaEstadistico/SistemaEstadistico/Form1.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace SistemaEstadistico
 {
@@ -15,6 +13,212 @@ namespace SistemaEstadistico
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Cargar las opciones en el ComboBox1
+            comboBox1.Items.Clear();
+            comboBox1.Items.Add("Edades");
+            comboBox1.Items.Add("Tiempo de traslado a la UGB");
+            comboBox1.Items.Add("Horas de uso del teléfono");
+
+            if (comboBox1.Items.Count > 0)
+                comboBox1.SelectedIndex = 0;
+
+            LimpiarTodo();
+        }
+
+        // Evento del Botón Calcular (button1)
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // 1. Validar que el TextBox1 de entrada no esté vacío
+                string entrada = textBox1.Text.Trim();
+                if (string.IsNullOrEmpty(entrada))
+                {
+                    MessageBox.Show("Por favor, ingrese los datos separados por comas.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                // 2. Convertir la cadena a lista de números
+                List<double> datos = entrada.Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                                           .Select(s => Convert.ToDouble(s.Trim()))
+                                           .OrderBy(n => n)
+                                           .ToList();
+
+                if (datos.Count == 0)
+                {
+                    MessageBox.Show("No se encontraron números válidos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                // 3. CÁLCULOS ESTADÍSTICOS
+
+                // Media Aritmética
+                double media = datos.Average();
+
+                // Mediana
+                double mediana;
+                int totalElementos = datos.Count;
+                if (totalElementos % 2 == 0)
+                {
+                    mediana = (datos[(totalElementos / 2) - 1] + datos[totalElementos / 2]) / 2.0;
+                }
+                else
+                {
+                    mediana = datos[totalElementos / 2];
+                }
+
+                // Moda
+                var frecuencias = datos.GroupBy(x => x)
+                                       .Select(g => new { Valor = g.Key, Conteo = g.Count() })
+                                       .ToList();
+
+                int maxFrecuencia = frecuencias.Max(f => f.Conteo);
+                string stringModa;
+
+                if (maxFrecuencia == 1 && datos.Count > 1)
+                {
+                    stringModa = "No hay moda";
+                }
+                else
+                {
+                    var modas = frecuencias.Where(f => f.Conteo == maxFrecuencia)
+                                           .Select(f => f.Valor.ToString("0.##"));
+                    stringModa = string.Join(", ", modas);
+                }
+
+                // Varianza
+                double sumaDiferenciasCuadrado = datos.Sum(d => Math.Pow(d - media, 2));
+                double varianza = totalElementos > 1 ? sumaDiferenciasCuadrado / (totalElementos - 1) : 0;
+
+                // Desviaciones
+                double desvEstandar = Math.Sqrt(varianza);
+                double desvTipica = desvEstandar;
+
+                // Rango
+                double rango = datos.Max() - datos.Min();
+
+                // 4. MOSTRAR RESULTADOS EN LOS TEXTBOX
+
+                textBox2.Text = media.ToString("F2");        // Media Aritmética
+                textBox3.Text = mediana.ToString("F2");      // Mediana
+                textBox4.Text = stringModa;                   // Moda
+                textBox5.Text = varianza.ToString("F2");     // Varianza
+                textBox6.Text = desvEstandar.ToString("F2"); // Desviación Estándar
+                textBox7.Text = desvTipica.ToString("F2");   // Desviación Típica
+                textBox8.Text = rango.ToString("F2");        // Rango
+
+                // 5. ACTUALIZAR TABLA Y GRÁFICO
+                LlenarTablaFrecuencia(frecuencias, totalElementos);
+                GraficarFrecuencia(frecuencias);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Asegúrese de ingresar únicamente números separados por comas.", "Error de formato", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocurrió un error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        // Método para construir la Tabla de Frecuencias en dataGridView1
+        private void LlenarTablaFrecuencia(dynamic frecuencias, int totalDatos)
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+
+            dataGridView1.Columns.Add("Valor", "Valor");
+            dataGridView1.Columns.Add("Frecuencia", "Frecuencia");
+            dataGridView1.Columns.Add("FrecuenciaRelativa", "Frecuencia Relativa");
+            dataGridView1.Columns.Add("FrecuenciaAcumulada", "Frecuencia Acumulada");
+
+            double frecAcumulada = 0;
+
+            foreach (var item in frecuencias)
+            {
+                double val = item.Valor;
+                int frecAbsoluta = item.Conteo;
+                double frecRelativa = (double)frecAbsoluta / totalDatos;
+                frecAcumulada += frecRelativa;
+
+                dataGridView1.Rows.Add(
+                    val.ToString("0.0"),
+                    frecAbsoluta,
+                    frecRelativa.ToString("F2"),
+                    frecAcumulada.ToString("F2")
+                );
+            }
+
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+        }
+
+        // Método para graficar en chart1
+        private void GraficarFrecuencia(dynamic frecuencias)
+        {
+            chart1.Series.Clear();
+            chart1.Titles.Clear();
+
+            Series series = new Series("Frecuencia")
+            {
+                ChartType = SeriesChartType.Column
+            };
+
+            foreach (var item in frecuencias)
+            {
+                series.Points.AddXY(item.Valor.ToString("0.0"), item.Conteo);
+            }
+
+            chart1.Series.Add(series);
+
+            if (chart1.ChartAreas.Count > 0)
+            {
+                chart1.ChartAreas[0].AxisX.Title = "Valor";
+                chart1.ChartAreas[0].AxisY.Title = "Frecuencia";
+                chart1.ChartAreas[0].AxisX.Interval = 1;
+            }
+        }
+
+        // Evento del Botón Limpiar (button2)
+        private void button2_Click(object sender, EventArgs e)
+        {
+            LimpiarTodo();
+        }
+
+        // Método para reiniciar todos los campos y controles
+        private void LimpiarTodo()
+        {
+            // Entrada de datos
+            textBox1.Clear();
+
+            // Resultados
+            textBox2.Text = "0.00"; // Media Aritmética
+            textBox3.Text = "0.00"; // Mediana
+            textBox4.Text = "-";    // Moda
+            textBox5.Text = "0.00"; // Varianza
+            textBox6.Text = "0.00"; // Desviación Estándar
+            textBox7.Text = "0.00"; // Desviación Típica
+            textBox8.Text = "0.00"; // Rango
+
+            // Tabla y Gráfico
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+
+            chart1.Series.Clear();
+            chart1.Titles.Clear();
+        }
+
+        // Filtro para aceptar solo números, comas, puntos y espacios en textBox1
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                e.KeyChar != ',' && e.KeyChar != '.' && e.KeyChar != ' ')
+            {
+                e.Handled = true;
+            }
         }
     }
 }
